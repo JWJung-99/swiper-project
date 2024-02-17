3f351c6c27cc8fd61e98eda61282cb71a2fef187
JWJung-99 <39busy@naver.com>
2024-02-17 13:53:41 +0900

    swiper practice

---
+++ main.js
@@ -0,0 +1,34 @@
+
+import { } from './lib/index.js';
+
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 2,
+  centeredSlides: true,
+  loop: true,
+  speed: 2000,
+  autoplay: {
+    delay: 3000,
+  },
+  effect: 'coverflow',
+  coverflowEffect: {
+    depth: 180,
+    rotate: 0,
+    slideShadows: false,
+    stretch: 20,
+  },
+  pagination: {
+    el: ".pagination",
+    clickable: true,
+    bulletClass: 'bullet',
+    bulletActiveClass: 'is-active',
+  },
+  navigation: {
+    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next",
+  },
+  parallax: true,
+  
+});
+
+
